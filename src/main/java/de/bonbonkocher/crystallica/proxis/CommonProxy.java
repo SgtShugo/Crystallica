c619b7bb5bca656c4eeceb3aac7da0f5653db47d
Bonbonkocher <johnnydepp1987@hotmail.de>
2015-01-23 01:44:02 +0100

    Add Crystalloca

---
+++ src/main/java/de/bonbonkocher/crystallica/proxis/CommonProxy.java
@@ -0,0 +1,12 @@
+package de.bonbonkocher.crystallica.proxis;
+
+import de.bonbonkocher.crystallica.Crystallica;
+
+public class CommonProxy extends Crystallica {
+
+	public void registerRenderers() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
